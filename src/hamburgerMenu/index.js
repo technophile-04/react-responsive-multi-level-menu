d40cd9890091f580275908a080fda2b41d29f7a1
Shiv Bhonde <shivbhonde04@gmail.com>
2021-11-28 17:16:48 +0530

    add text props to hamburget

--- src/hamburgerMenu/index.js
+++ src/hamburgerMenu/index.js
@@ -1,22 +1,20 @@
-import React from "react"; // eslint-disable-line
+import React from 'react'; // eslint-disable-line
 import PropTypes from 'prop-types';
 
-const HamBurgerMenu = ({ style, showItemsHandler, color, lineColor }) => (
+const HamBurgerMenu = ({ style, showItemsHandler, color, lineColor, text }) => (
   <div
     className={`burger-menu ${style}`}
     onClick={showItemsHandler}
     style={{ backgroundColor: color }}
   >
-    <div className="line" style={{ backgroundColor: lineColor }} />
-    <div className="line" style={{ backgroundColor: lineColor }} />
-    <div className="line" style={{ backgroundColor: lineColor }} />
+    <div style={{ backgroundColor: lineColor }}>{text}</div>
   </div>
 );
 HamBurgerMenu.propTypes = {
   showItemsHandler: PropTypes.func.isRequired,
   color: PropTypes.string.isRequired,
   style: PropTypes.string.isRequired,
-  lineColor: PropTypes.string.isRequired
+  lineColor: PropTypes.string.isRequired,
 };
 
 export default HamBurgerMenu;
